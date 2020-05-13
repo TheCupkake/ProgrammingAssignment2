5a36489ea0306d43eed6c9e08bdfa0d649ac27b9
Eyad Abu-Zaid <33122334+TheCupkake@users.noreply.github.com>
2020-05-13 13:35:58 +0200

    script written for programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R script consists of two functions that, combined can evaluate
+## the inverse of a square matrix, the matrix for which the inverse
+## is being evaluated is always assumed to be a square matrix
 
-## Write a short comment describing this function
+
+## A function that takes a matrix object as input, and creates a list of 
+## four functions, get(), set(), get_inverse() and set_inverse() for getting
+## and setting the matrix itself and getting and setting the inverse of 
+## the matrix respectively
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  minv <- NULL
+  set <- function(y){
+    x <<- y
+    minv <<- NULL
+  }
+  get <- function() x
+  set_inverse <- function(inverse) minv <<- inverse 
+  get_inverse <- function() minv
+  list(set = set, get = get,
+       set_inverse = set_inverse,
+       get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
+## a function for calculating the inverse of a matrix, and caching the value
+## so that when it's called again it doesn't re-evaluate the inverse and instead
+## simply retrieve the data from cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  minv <- x$get_inverse()
+  if (!is.null(minv)){
+    message("getting cached data")
+    return(minv)
+  }
+  data <- x$get()
+  minv <- solve(data,...)
+  x$set_inverse(minv)
+  minv
+  
 }
